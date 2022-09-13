418cce320547402b59ac1e4a93b39a72d4a2491e
Amin Elayan <ameen.m.elayyan@gmail.com>
2022-09-13 19:31:54 +0300

    update

--- _python/python_fundamentals/Bank/Bank.py
+++ _python/python_fundamentals/Bank/Bank.py
@@ -8,7 +8,10 @@ class BankAccount:
         self.balance += amount
         return self
     def withdraw(self, amount):
-        self.balance -= amount
+        if self.balance<amount:
+                print("Insufficient funds: Charging a $5 fee and deduct $5")
+                self.balance-=5
+        else: self.balance -= amount
         return self
     def display_account_info(self):
         print("account number:",self.account_number,"intereset rate: ",self.int_rate,"account balance: ",self.balance)
@@ -20,11 +23,15 @@ class BankAccount:
             self.balance += 0
         return self
 
-n111 =BankAccount(778,2500)
+n111 =BankAccount(778,2000)
 n112 =BankAccount(753,2500)
 
  # To the first account, make 3 deposits and 1 withdrawal, then yield interest and display the account's info all in one line of code (i.e. chaining)
-n111.deposit(100).deposit(200).deposit(300).withdraw(400).yield_interest().display_account_info()
+n111.withdraw(2800).yield_interest().display_account_info()
 
  # To the second account, make 2 deposits and 4 withdrawals, then yield interest and display the account's info all in one line of code (i.e. chaining)
 n112.deposit(400).deposit(100).withdraw(200).withdraw(100).withdraw(50).yield_interest().display_account_info()
+
+
+
+#meshan allah 3lmne el code ):
