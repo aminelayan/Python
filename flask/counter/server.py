c1d322a8ba7983ce71a40564dbf3e486c6afbc62
Amin Elayan <ameen.m.elayyan@gmail.com>
2022-09-19 10:35:07 +0300

    counter

---
+++ flask/counter/server.py
@@ -0,0 +1,27 @@
+from flask import Flask , render_template, redirect, session
+
+app=Flask(__name__)
+app.secret_key='amin'
+
+@app.route('/')
+def index():
+    if 'count' not in session:
+        session['count']=0
+    return render_template("index.html", count= session['count'])
+
+@app.route('/addone')
+def addone():
+    session['count']+=1
+    return redirect('/')
+
+@app.route('/addtwo')
+def addtwo():
+    session['count']+=2
+    return redirect('/')
+@app.route('/reset')
+def reset():
+    session['count']=0
+    return redirect('/')
+
+
+app.run(debug=True)
